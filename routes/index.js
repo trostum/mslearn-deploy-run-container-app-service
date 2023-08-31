c4434163c7b639765b274abe3f81f15105b2df2b
Terje Røstum <terje.rostum@entur.org>
2023-08-31 07:43:03 +0200

    Endre tittel, trigge bygg automatisk

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express FTW! Manual code change' });
+  res.render('index', { title: 'Express FTW! BOO YAH! Deployed by commit' });
 });
 
 module.exports = router;
